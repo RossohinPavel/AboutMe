5db248accb7d85c48e5bdf49746a12b0ab5ce33c
dfx_17 <rossohinpavel@yandex.ru>
2026-06-06 13:49:16 +0300

    header done

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,16 @@
+import styles from './Container.module.css';
+
+
+export interface ContainerProps {
+  children: React.ReactNode
+}
+
+export function Container(props: ContainerProps) {
+  const { children } = props;
+
+  return (
+    <div className={styles["container"]}>
+      {children}
+    </div>
+  );
+}
---
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import MoonIcon from "../../assets/icon-moon.svg";
+import SunIcon from "../../assets/icon-sun.svg";
+import styles from './ThemeSwitcher.module.css';
+
+
+export function ThemeSwitcher() {
+  const [isDark, setDark] = useState(false);
+
+  const themeText = isDark ? "Dark" : "Light";
+  const ThemeIcon = isDark ? MoonIcon : SunIcon;
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light')
+  }, [isDark])
+
+  return (
+    <div 
+      className={styles["theme-switcher"]}
+      onClick={() => setDark(!isDark)}
+    >
+      <span>{themeText}</span>
+      <img 
+        src={ThemeIcon} 
+        alt="Theme Icon" 
+        className={styles["icon"]}
+      />
+    </div>
+  );
+}
