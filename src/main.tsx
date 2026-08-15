4b53c5f65e9d27b5321dc31a72e986590f86bb66
df17 <rossohinpavel@yandex.ru>
2026-08-15 13:56:07 +0300

    feat: add theme-switching header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './main.scss';
+import { Header } from "./components/Header/Header";
+import "./main.scss";
 
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("header")!).render(
   <StrictMode>
-    {/* <App /> */}
+    <Header />
   </StrictMode>,
 );
